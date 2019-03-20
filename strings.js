2e2f82f59a5ea0bcb6f86a1a51ca579e28b04075
RBK-21 <rbk-21@Mac-mini-5.Dlink>
2019-03-20 09:55:42 +0200

    “areej”

--- strings.js
+++ strings.js
@@ -3,5 +3,9 @@
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
 	function repeatStr (n, s) {
-		//your code is here
+		var str = "" ; 
+for (var i = 0; i < n; i++) {
+	str = str + s ;
+}
+return str ; 
 	}
